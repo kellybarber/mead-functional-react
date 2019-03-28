a5bfb96c2e7d59473209d13f056bde933a3992cd
Kelly Barber <Kelly@Kellys-MacBook-Air.local>
2019-03-27 20:51:40 -0600

    # Added gitignore and babelrc

--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -1,29 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Counter from '../../components/Counter/Counter';
 
-export default ({ initialCount = 0 }) => {
-  const [ count, setCount ] = useState(initialCount)
-
-  const buttonStyle = {
-    margin       : '0 20px',
-    padding      : '10px',
-    borderRadius : '5px',
-    cursor       : 'pointer'
-  }
-
-  return (
-    <div>
-      <h1 style={{ margin : '20px' }} >
-        Hello, this is count: {count}
-      </h1>
-      <button style={ buttonStyle } onClick={() => setCount(count + 1)}>
-        Increment Me
-      </button>
-      <button style={ buttonStyle } onClick={() => count > 0 && setCount(count - 1)}>
-        Decrement Me
-      </button>
-      <button style={ buttonStyle } onClick={() => setCount(0)}>
-        Reset Me
-      </button>
-    </div>
-  )
-}
+export default () => (
+  <Counter/>
+)
